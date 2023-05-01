39d616a8440c8916ea9f81a36ea9edaf219d7ad6
woojin <sun65568@hanmail.net>
2023-05-01 17:11:51 +0900

    Initial commit

---
+++ src/main/java/com/kh/mini_sample_project/MiniSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kh.mini_sample_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSampleProjectApplication.class, args);
+	}
+
+}
